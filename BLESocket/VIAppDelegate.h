0fef5b88229170b1895149e1e024d9f0517e3f55
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2014-08-16 10:25:25 +0300

    Initial commit

---
+++ BLESocket/VIAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  VIAppDelegate.h
+//  BLESocket
+//
+//  Created by Victor Ilyukevich on 8/16/14.
+//  Copyright (c) 2014 Victor Ilyukevich. All rights reserved.
+//
+
+@interface VIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BLESocket/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BLESocket
+//
+//  Created by Victor Ilyukevich on 8/16/14.
+//  Copyright (c) 2014 Victor Ilyukevich. All rights reserved.
+//
+
+#import "VIAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([VIAppDelegate class]));
+  }
+}
