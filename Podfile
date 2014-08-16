d63557485c24e8e2825d8c8465eebff44b6b383c
Victor Ilyukevich <ilyukevich.victor@gmail.com>
2014-08-16 12:51:58 +0300

    Added BlocksKit

--- Podfile
+++ Podfile
@@ -1,6 +1,7 @@
 platform :ios, '7.0'
 
 target 'BLESocket' do
+  pod 'BlocksKit'
   pod 'CocoaLumberjack'
 end
 
---
+++ Pods/Pods-BLESocket-BlocksKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BLESocket_BlocksKit : NSObject
+@end
+@implementation PodsDummy_Pods_BLESocket_BlocksKit
+@end
